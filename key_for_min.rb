98953d33921f75e214c9686e5d86afff6d8d4544
prodigysurge <boatinglife6@yahoo.com>
2021-01-15 10:51:32 -0500

    commit tree 630c9c840713b5aac9bc662146946d7f3cda55eb

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,15 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(students_age)
+        low_key = nil
+        low_val = nil
+    students_age.each do |k,v|
+        if low_val == nil || v < low_val
+            low_val = v
+            low_key = k 
+        end
+    
+    end
+    low_key
 end
